81f3de611cf5821880bd7a92061e6f768e122c79
Mayank_Loriya <mayank.l@simformsolutions.com>
2022-10-21 18:12:54 +0530

    UNT-T10887 | admin panel UI in progress

--- public/app.js
+++ public/app.js
@@ -87,7 +87,7 @@ tabTogglers.forEach(function(toggler) {
 });
 
 // menu dropdown
-indow.Components = {
+window.Components = {
     customSelect(options) {
       return {
         init() {
@@ -145,4 +145,449 @@ indow.Components = {
       }
     },
   }
-  
+
+//charts
+  const chartOptions = {
+    maintainAspectRatio: false,
+    legend: {
+        display: false,
+    },
+    tooltips: {
+        enabled: false,
+    },
+    elements: {
+        point: {
+            radius: 0
+        },
+    },
+    scales: {
+        xAxes: [{
+            gridLines: false,
+            scaleLabel: false,
+            ticks: {
+                display: false
+            }
+        }],
+        yAxes: [{
+            gridLines: false,
+            scaleLabel: false,
+            ticks: {
+                display: false,
+                suggestedMin: 0,
+                suggestedMax: 10
+            }
+        }]
+    }
+};
+//
+var ctx = document.getElementById('chart1').getContext('2d');
+var chart = new Chart(ctx, {
+    type: "line",
+    data: {
+        labels: [1, 2, 1, 3, 5, 4, 7],
+        datasets: [
+            {
+                backgroundColor: "rgba(101, 116, 205, 0.1)",
+                borderColor: "rgba(101, 116, 205, 0.8)",
+                borderWidth: 2,
+                data: [1, 2, 1, 3, 5, 4, 7],
+            },
+        ],
+    },
+    options: chartOptions
+});
+//
+var ctx = document.getElementById('chart2').getContext('2d');
+var chart = new Chart(ctx, {
+    type: "line",
+    data: {
+        labels: [2, 3, 2, 9, 7, 7, 4],
+        datasets: [
+            {
+                backgroundColor: "rgba(246, 109, 155, 0.1)",
+                borderColor: "rgba(246, 109, 155, 0.8)",
+                borderWidth: 2,
+                data: [2, 3, 2, 9, 7, 7, 4],
+            },
+        ],
+    },
+    options: chartOptions
+});
+//
+var ctx = document.getElementById('chart3').getContext('2d');
+var chart = new Chart(ctx, {
+    type: "line",
+    data: {
+        labels: [2, 5, 1, 3, 2, 6, 7],
+        datasets: [
+            {
+                backgroundColor: "rgba(246, 153, 63, 0.1)",
+                borderColor: "rgba(246, 153, 63, 0.8)",
+                borderWidth: 2,
+                data: [2, 5, 1, 3, 2, 6, 7],
+            },
+        ],
+    },
+    options: chartOptions
+});
+
+
+function app() {
+  return {
+    chartData: [112, 10, 225, 134, 101, 80, 50, 100, 200],
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+    tooltipContent: '',
+    tooltipOpen: false,
+    tooltipX: 0,
+    tooltipY: 0,
+    showTooltip(e) {
+      console.log(e);
+      this.tooltipContent = e.target.textContent
+      this.tooltipX = e.target.offsetLeft - e.target.clientWidth;
+      this.tooltipY = e.target.clientHeight + e.target.clientWidth;
+    },
+    hideTooltip(e) {
+      this.tooltipContent = '';
+      this.tooltipOpen = false;
+      this.tooltipX = 0;
+      this.tooltipY = 0;
+    }
+  }
+}
+
+Number.prototype.m_formatter = function() {
+  return this > 999999 ? (this / 1000000).toFixed(1) + 'M' : this
+};
+let stockTicker = function(){
+  return {
+      stockFullName: 'SW Limited.',
+      stockShortName: 'ASX:SFW',
+      price: {
+          current: 2.320,
+          open: 2.230,
+          low: 2.215,
+          high: 2.325,
+          cap: 93765011,
+          ratio: 20.10,
+          dividend: 1.67
+      },
+      chartData: {
+          labels: ['10:00','','','','12:00','','','','2:00','','','','4:00'],
+          data: [2.23,2.215,2.22,2.25,2.245,2.27,2.28,2.29,2.3,2.29,2.325,2.325,2.32],
+      },
+      renderChart: function(){
+          let c = false;
+
+          Chart.helpers.each(Chart.instances, function(instance) {
+              if (instance.chart.canvas.id == 'chart') {
+                  c = instance;
+              }
+          });
+
+          if(c) {
+              c.destroy();
+          }
+
+          let ctx = document.getElementById('chart').getContext('2d');
+
+          let chart = new Chart(ctx, {
+              type: "line",
+              data: {
+                  labels: this.chartData.labels,
+                  datasets: [
+                      {
+                          label: '',
+                          backgroundColor: "rgba(255, 255, 255, 0.1)",
+                          borderColor: "rgba(255, 255, 255, 1)",
+                          pointBackgroundColor: "rgba(255, 255, 255, 1)",
+                          data: this.chartData.data,
+                      },
+                  ],
+              },
+              layout: {
+                  padding: {
+                      right: 10
+                  }
+              },
+              options: {
+                  legend: {
+                      display: false,
+                  },
+                  scales: {
+                      yAxes: [{
+                          ticks: {
+                              fontColor: "rgba(255, 255, 255, 1)",
+                          },
+                          gridLines: {
+                              display: false,
+                          },
+                      }],
+                      xAxes: [{
+                          ticks: {
+                              fontColor: "rgba(255, 255, 255, 1)",
+                          },
+                          gridLines: {
+                              color: "rgba(255, 255, 255, .2)",
+                              borderDash: [5, 5],
+                              zeroLineColor: "rgba(255, 255, 255, .2)",
+                              zeroLineBorderDash: [5, 5]
+                          },
+                      }]
+                  }
+              }
+          });
+      }
+  }
+}
+// Bar chart - echart
+// Initialize the echarts instance based on the prepared dom
+var myBarChart = echarts.init(document.getElementById('BarChart'));
+
+// Specify the configuration items and data for the chart
+var option = {
+  title: {
+    text: 'ECharts Getting Started Example'
+  },
+  tooltip: {},
+  legend: {
+    data: ['sales']
+  },
+  xAxis: {
+    data: ['Shirts', 'Cardigans', 'Chiffons', 'Pants', 'Heels', 'Socks']
+  },
+  yAxis: {},
+  series: [
+    {
+      name: 'sales',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20]
+    }
+  ]
+};
+
+// Display the chart using the configuration items and data just specified.
+myBarChart.setOption(option);
+window.addEventListener('resize', myBarChart.resize);
+
+// Line chart - echart
+// Initialize the echarts instance based on the prepared dom
+var domLine = document.getElementById('LineChart');
+var myLineChart = echarts.init(domLine, null, {
+  renderer: 'canvas',
+  useDirtyRect: false
+});
+var app = {};
+
+var option;
+
+option = {
+  title: {
+    text: 'Stacked Line'
+  },
+  tooltip: {
+    trigger: 'axis'
+  },
+  legend: {
+    data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true
+  },
+  toolbox: {
+    feature: {
+      saveAsImage: {}
+    }
+  },
+  xAxis: {
+    type: 'category',
+    boundaryGap: false,
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+  },
+  yAxis: {
+    type: 'value'
+  },
+  series: [
+    {
+      name: 'Email',
+      type: 'line',
+      stack: 'Total',
+      data: [120, 132, 101, 134, 90, 230, 210]
+    },
+    {
+      name: 'Union Ads',
+      type: 'line',
+      stack: 'Total',
+      data: [220, 182, 191, 234, 290, 330, 310]
+    },
+    {
+      name: 'Video Ads',
+      type: 'line',
+      stack: 'Total',
+      data: [150, 232, 201, 154, 190, 330, 410]
+    },
+    {
+      name: 'Direct',
+      type: 'line',
+      stack: 'Total',
+      data: [320, 332, 301, 334, 390, 330, 320]
+    },
+    {
+      name: 'Search Engine',
+      type: 'line',
+      stack: 'Total',
+      data: [820, 932, 901, 934, 1290, 1330, 1320]
+    }
+  ]
+};
+
+if (option && typeof option === 'object') {
+  myLineChart.setOption(option);
+}
+
+window.addEventListener('resize', myLineChart.resize);
+
+// Doughnut chart - echart
+// Initialize the echarts instance based on the prepared dom
+var domDoughnut = document.getElementById('DoughnutChart');
+var myDoughnutChart = echarts.init(domDoughnut, null, {
+  renderer: 'canvas',
+  useDirtyRect: false
+});
+var app = {};
+
+var option;
+
+option = {
+  tooltip: {
+    trigger: 'item'
+  },
+  legend: {
+    top: '5%',
+    left: 'center'
+  },
+  series: [
+    {
+      name: 'Access From',
+      type: 'pie',
+      radius: ['40%', '70%'],
+      avoidLabelOverlap: false,
+      label: {
+        show: false,
+        position: 'center'
+      },
+      emphasis: {
+        label: {
+          show: true,
+          fontSize: '40',
+          fontWeight: 'bold'
+        }
+      },
+      labelLine: {
+        show: false
+      },
+      data: [
+        { value: 1048, name: 'Search Engine' },
+        { value: 735, name: 'Direct' },
+        { value: 580, name: 'Email' },
+        { value: 484, name: 'Union Ads' },
+        { value: 300, name: 'Video Ads' }
+      ]
+    }
+  ]
+};
+
+if (option && typeof option === 'object') {
+  myDoughnutChart.setOption(option);
+}
+
+window.addEventListener('resize', myDoughnutChart.resize);
+
+// Bar with Negativevalue Chart - echart
+// Initialize the echarts instance based on the prepared dom
+var domBarwithNegativeValue = document.getElementById('BarwithNegativeChart');
+var myBarNegativeChart = echarts.init(domBarwithNegativeValue, null, {
+  renderer: 'canvas',
+  useDirtyRect: false
+});
+var app = {};
+
+var option;
+
+const labelRight = {
+  position: 'right'
+};
+option = {
+  title: {
+    text: 'Bar Chart with Negative Value'
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow'
+    }
+  },
+  grid: {
+    top: 80,
+    bottom: 30
+  },
+  xAxis: {
+    type: 'value',
+    position: 'top',
+    splitLine: {
+      lineStyle: {
+        type: 'dashed'
+      }
+    }
+  },
+  yAxis: {
+    type: 'category',
+    axisLine: { show: false },
+    axisLabel: { show: false },
+    axisTick: { show: false },
+    splitLine: { show: false },
+    data: [
+      'ten',
+      'nine',
+      'eight',
+      'seven',
+      'six',
+      'five',
+      'four',
+      'three',
+      'two',
+      'one'
+    ]
+  },
+  series: [
+    {
+      name: 'Cost',
+      type: 'bar',
+      stack: 'Total',
+      label: {
+        show: true,
+        formatter: '{b}'
+      },
+      data: [
+        { value: -0.07, label: labelRight },
+        { value: -0.09, label: labelRight },
+        0.2,
+        0.44,
+        { value: -0.23, label: labelRight },
+        0.08,
+        { value: -0.17, label: labelRight },
+        0.47,
+        { value: -0.36, label: labelRight },
+        0.18
+      ]
+    }
+  ]
+};
+
+if (option && typeof option === 'object') {
+  myBarNegativeChart.setOption(option);
+}
+
+window.addEventListener('resize', myBarNegativeChart.resize);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,140 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
 module.exports = {
   content: ["./public/*.{html,js}"],
   darkMode: 'class',
   mode: 'jit',
-  theme: {
-    extend: {
-    },
-  },
   plugins: [
     require('@tailwindcss/forms'),
-  ],
+    plugin(function ({ addComponents }) {
+      addComponents({
+        '.button-save': {
+          backgroundColor: 'rgb(37 99 235)',
+          color: '#ffffff',
+          display: 'inline-flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '0.5rem 1rem',
+          fontSize: '0.875rem',
+          lineHeight: '1.25rem',
+          fontWeight: '500',
+          border: '1px solid transparent',
+          borderRadius: '0.375rem',
+          boxShadow: '0 1px 2px 0 rgb(0 0 0 / 0.05)',
+          fontWeight: '500',
+          transitionDuration: '200ms',
+          transitionProperty: 'color, background-color, border-color, text-decoration-color, fill, stroke, opacity, box-shadow, transform, filter, backdrop-filter, -webkit-backdrop-filter',
+          transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
+          '&:hover': {
+            backgroundColor: 'rgb(29 78 216)',
+          },
+          '&:focus': {
+            outline: '2px solid transparent',
+            outlineOffset: '2px'
+          }
+        },
+        '.btn-outline': {
+          display: 'inline-flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          lineHeight: '1rem',
+          color: 'rgb(55 65 81)',
+          fontWeight: '400',
+          boxShadow: '0 1px 2px 0 rgb(0 0 0 / 0.05)',
+          fontSize: '0.875rem',
+          padding: '0.5rem 1rem',
+          backgroundColor: '#ffffff',
+          borderColor: 'rgb(209 213 219)',
+          borderWidth: '2px',
+          borderRadius: '0.375rem',
+          transitionDuration: '200ms',
+          transitionProperty: 'color, background-color, border-color, text-decoration-color, fill, stroke, opacity, box-shadow, transform, filter, backdrop-filter, -webkit-backdrop-filter',
+          transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
+          '&:hover': {
+            backgroundColor: 'rgb(249 250 251)',
+          },
+          '&:focus': {
+            outline: '2px solid transparent',
+            outlineOffset: '2px',
+            borderColor: 'rgb(209 213 219)',
+          }
+        },
+        '.form-label': {
+          fontWeight: '500',
+          color: 'rgb(55 65 8)',
+          fontSize: '0.875rem',
+          lineHeight: '1.25rem',
+          display: 'block'
+        },
+        '.form-input':{
+          fontSize: '0.875rem',
+          lineHeight: '1.25rem',
+          boxShadow: '0 1px 2px 0 rgba(0 0 0 0.05)',
+          borderColor: 'rgb(209 213 219)',
+          borderRadius: '0.375rem',
+          width: '100%',
+          display: 'block',
+          marginTop: '0.25rem',
+          '&:focus':{
+            outline: '2px solid transparent',
+            outlineOffset: '2px',
+            borderColor: 'rgb(29 78 216)',
+          }
+        },
+        '.form-checkbox':{
+          color: 'rgb(37 99 235)',
+          borderRadius: '0.25rem',
+          width: '1rem',
+          height: '1rem',
+        },
+        '.form-radio':{
+          color: 'rgb(37 99 235)',
+          width: '1rem',
+          height: '1rem',
+        },
+        '.button-light-gray':{
+          color: ' rgb(75 85 99)',
+          fontSize: '0.875rem',
+          lineHeight: '1',
+          padding: '0.75rem 1.25rem',
+          backgroundColor: 'rgb(243 244 246)',
+          borderRadius: '0.25rem',
+          display: 'inline-flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderColor: 'rgb(229 231 235)',
+          borderWidth: '0',
+          '&:hover': {
+            backgroundColor: 'rgb(229 231 235)',
+          },
+          '&:focus': {
+            outline: '0px solid transparent',
+            outlineOffset: '0px',
+            backgroundColor: 'rgb(229 231 235)',
+          }
+        },
+        '.button-cancel':{
+          color: 'rgb(55 65 81)',
+          fontSize: '1.125rem',
+          lineHeight: '1.75rem',
+          padding: '0.5rem 1.5rem',
+          backgroundColor: 'rgb(229 231 235)',
+          borderWidth: '0px',
+          borderRadius: '0.25rem',
+          display: 'inline-flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          '&:hover': {
+            backgroundColor: 'rgb(209 213 219)',
+          },
+          '&:focus': {
+            outline: '0px solid transparent',
+            outlineOffset: '0px',
+            backgroundColor: 'rgb(209 213 219)',
+          }
+        }
+      })
+     }
+    )
+  ]
 }
